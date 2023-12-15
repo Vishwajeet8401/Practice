ccf009e2ccc20f852e89962351ac6bcea8355243
vishwajeet <vishwk8401@gmail.com>
2023-12-15 10:07:25 +0530

    initaital commit

---
+++ progra.java
@@ -0,0 +1,23 @@
+public class program{
+    public static void main(String args[]){
+        int a[]=new int [100];
+        Boolean b[]=new Boolean [100];
+        int c[]=;
+        
+
+        for(int i=0;i<a.length;i++){
+            a[i]=i+1;
+            b[i]=false;
+            
+        }
+
+
+
+        
+
+
+
+
+
+    }
+}
