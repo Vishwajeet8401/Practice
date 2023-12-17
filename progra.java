5771d43826e0b0b1921af6c6e075d99ed368e6a6
vishwajeet <vishwk8401@gmail.com>
2023-12-17 12:10:15 +0530

    add some style for resposnsive

--- progra.java
+++ progra.java
@@ -1,23 +1,26 @@
-public class program{
-    public static void main(String args[]){
-        int a[]=new int [100];
-        Boolean b[]=new Boolean [100];
-        int c[]=;
-        
+public class progra {
+    public static void main(String args[]) {
 
-        for(int i=0;i<a.length;i++){
-            a[i]=i+1;
-            b[i]=false;
-            
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if (i >= j || i + j >= 11) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }
-
-
-
-        
-
-
-
-
-
+        for (int i = 2; i <= 5; i++) {
+            for (int j = 10; j > 0; j--) {
+                if (j >= 5 + i || j <= 6 - i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
+
 }
